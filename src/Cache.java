da925e0e59a035597e3c1c88104ca69c7c460a57
YigitTuncer07 <yigit_tuncer03@hotmail.com>
2023-05-27 18:21:46 +0300

    Initial Commit

--- src/Cache.java
+++ src/Cache.java
@@ -4,30 +4,40 @@ import java.util.Scanner;
 
 public class Cache {
 
-    private static String cache[][];
+    private static Line cache[][];
+    private static int s;
+    private static int E;
+    private static int b;
 
     public static void main(String[] args) throws FileNotFoundException {
 
         // Calculates S,E,B according to the input
-        int s = Integer.parseInt(args[1]);
+        s = Integer.parseInt(args[1]);
         int S = (int) Math.pow(2, s);
-        int E = Integer.parseInt(args[3]);
-        int b = Integer.parseInt(args[5]);
+        E = Integer.parseInt(args[3]);
+        b = Integer.parseInt(args[5]);
         int B = (int) Math.pow(2, b);
         File traceFile = new File("traces/" + args[7]);
 
+
         System.out.println("S = " + S + "||E = " + E + "||B = " + B + "||File : " + traceFile.exists() + "\n");
 
-        // Our cache is a 2D array, where sets are the first dimension and lines the
-        // second
-        cache = new String[S][E];
+        // Our cache is a 2D array of lines
+        cache = new Line[S][E];
+
+        //Instantiate the cache
+        for (int i = 0; i < S; i++){
+            for (int j = 0; j < E; j++){
+                cache[i][j] = new Line();
+            }
+        }
 
         Scanner scanner = new Scanner(traceFile);
         String currentCommand;
         boolean isHit;
 
-
-        //Commands are composed of an address, a int representing size, and sometimes some data
+        // Commands are composed of an address, a int representing size, and sometimes
+        // some data
         char commandType;
         String address;
         int size;
@@ -37,24 +47,23 @@ public class Cache {
 
             currentCommand = scanner.nextLine();
 
-            //This part parses the command
+            // This part parses the command
             commandType = currentCommand.charAt(0);
-            address = currentCommand.substring(2,10);
+            address = currentCommand.substring(2, 10);
             size = Character.getNumericValue(currentCommand.charAt(12));
-            if (commandType != 'L'){
+            if (commandType != 'L') {
                 data = currentCommand.substring(15);
             }
 
-            System.out.println("Command Type: " + commandType);
-            System.out.println("Address: " + address);
-            System.out.println("Size: " + size);
-            System.out.println("Data: " + data + "\n");
-
+            // System.out.println("Command Type: " + commandType);
+            // System.out.println("Address: " + address);
+            // System.out.println("Size: " + size);
+            // System.out.println("Data: " + data + "\n");
 
             switch (commandType) {
                 case 'L':
-                    isHit = checkCache("temp");
-                    if (isHit == true){
+                    isHit = checkCache(address, s, b, E);
+                    if (isHit == true) {
 
                     }
 
@@ -70,8 +79,54 @@ public class Cache {
         scanner.close();
     }
 
-    private static boolean checkCache(String address){
+    // s is how many bits the set part is, and b is how many bits the block offset
+    // part is. E is how many lines there are per set
+    private static boolean checkCache(String address, int s, int b, int E) {
 
-        return true;
+        address = hexToBinary(address);
+
+        int t = 32 - s - b;// How many bits the tag is
+        String tag;
+        int setIndex;
+        int blockOffset;
+
+        // Now, we must parse the address
+        blockOffset = binaryToInteger(address.substring(t + s));
+        setIndex = binaryToInteger(address.substring(t, t + s + 1));
+        tag = address.substring(0, 32 - s - b);
+        System.out.println(tag);
+
+        Line currentSet[] = cache[setIndex];
+
+        // Now, we search for our tag
+        for (Line line : currentSet) {
+            if ((line.getTag().equals(tag))) {
+                if (line.getValidBit() == 1) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static int binaryToInteger(String string) {
+        int result = 0;
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) == '1') {
+                result += Math.pow(2, string.length() - i - 1);
+            }
+
+        }
+        return result;
+    }
+
+    private static String hexToBinary(String hexString) {
+        int decimal = Integer.parseInt(hexString, 16);
+        String binaryString = Integer.toBinaryString(decimal);
+        StringBuilder paddedBinary = new StringBuilder(binaryString);
+        while (paddedBinary.length() < 32) {
+            paddedBinary.insert(0, "0");
+        }
+        return paddedBinary.toString();
     }
 }
---
+++ src/Line.java
@@ -0,0 +1,35 @@
+public class Line {
+    byte validBit;
+    String tag;
+    String data;
+
+    public Line (){
+        tag = "";
+        data = "";
+        validBit = 0;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public byte getValidBit() {
+        return validBit;
+    }
+
+    public void setValidBit(byte validBit) {
+        this.validBit = validBit;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
