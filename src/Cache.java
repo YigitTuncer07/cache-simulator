b01da6a2ac5c3d0589febdee6ca5556a1b4c34a7
YigitTuncer07 <yigit_tuncer03@hotmail.com>
2023-05-29 14:31:00 +0300

    Added load funtion

--- src/Cache.java
+++ src/Cache.java
@@ -1,17 +1,51 @@
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Cache {
 
+    private static ArrayList<String> ram = new ArrayList<>();
     private static Line cache[][];
+    private static Queue<Line> queues[];
     private static int s;
     private static int E;
     private static int b;
+    private static int hits;
+    private static int misses;
+    private static int evictions;
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
 
-        // Calculates S,E,B according to the input
+        // Loads the ram into memory
+        InputStream inputStream = new FileInputStream("RAM.dat");
+        int mem = 0;
+        int byteRead = -1;
+        String currentHex = "";
+        String currentString = "";
+        while ((byteRead = inputStream.read()) != -1) {
+
+            currentString = intToHex(byteRead);
+            mem++;
+
+            if (currentString.length() == 1) {// Pads with 0 if 00
+                currentString = "0" + currentString;
+            }
+
+            currentHex = currentHex + currentString;
+
+            if (mem % 8 == 0) {
+                ram.add(currentHex);
+                currentHex = "";
+            }
+        }
+        inputStream.close();
+
+        // Calculates S,E,B according to the command line arguments
         s = Integer.parseInt(args[1]);
         int S = (int) Math.pow(2, s);
         E = Integer.parseInt(args[3]);
@@ -19,17 +53,23 @@ public class Cache {
         int B = (int) Math.pow(2, b);
         File traceFile = new File("traces/" + args[7]);
 
-
         System.out.println("S = " + S + "||E = " + E + "||B = " + B + "||File : " + traceFile.exists() + "\n");
 
         // Our cache is a 2D array of lines
         cache = new Line[S][E];
 
-        //Instantiate the cache
-        for (int i = 0; i < S; i++){
-            for (int j = 0; j < E; j++){
+        // Instantiate the cache
+        for (int i = 0; i < S; i++) {
+            for (int j = 0; j < E; j++) {
                 cache[i][j] = new Line();
             }
+        }
+
+        // Create queues for the sets of the cache, so that we know which line to evict.
+        queues = new Queue[S];
+
+        for (int i = 0; i < S; i++) {
+            queues[i] = new PriorityQueue<Line>();
         }
 
         Scanner scanner = new Scanner(traceFile);
@@ -62,10 +102,8 @@ public class Cache {
 
             switch (commandType) {
                 case 'L':
-                    isHit = checkCache(address, s, b, E);
-                    if (isHit == true) {
 
-                    }
+                    dataLoad(address, size);
 
                     break;
                 case 'S':
@@ -81,9 +119,9 @@ public class Cache {
 
     // s is how many bits the set part is, and b is how many bits the block offset
     // part is. E is how many lines there are per set
-    private static boolean checkCache(String address, int s, int b, int E) {
+    private static void dataLoad(String address, int size) {
 
-        address = hexToBinary(address);
+        String binAddress = hexToBinary(address);
 
         int t = 32 - s - b;// How many bits the tag is
         String tag;
@@ -91,22 +129,60 @@ public class Cache {
         int blockOffset;
 
         // Now, we must parse the address
-        blockOffset = binaryToInteger(address.substring(t + s));
-        setIndex = binaryToInteger(address.substring(t, t + s + 1));
-        tag = address.substring(0, 32 - s - b);
+        blockOffset = binaryToInteger(binAddress.substring(t + s));
+        setIndex = binaryToInteger(binAddress.substring(t, t + s + 1));
+        tag = binAddress.substring(0, 32 - s - b);
         System.out.println(tag);
 
+        // Get current set
         Line currentSet[] = cache[setIndex];
 
-        // Now, we search for our tag
+        // Now, we search for our tag in the lines of the set
         for (Line line : currentSet) {
-            if ((line.getTag().equals(tag))) {
-                if (line.getValidBit() == 1) {
-                    return true;
+            if (line.getValidBit() == 1) {
+                if ((line.getTag().equals(tag))) {
+                    hits++;
+                    return;
                 }
             }
         }
-        return false;
+
+        // The rest of the function only executes if a miss occured.
+        String data = accessRam(address);
+
+        for (Line line : currentSet) {
+            // If the valid bit of a line is 0, we can just write to there without evicting
+            if (line.getValidBit() == 0) {
+                line.setValidBit((byte) 1);
+                line.setTag(tag);
+                line.setData(data);
+                queues[setIndex].add(line);
+                return;
+            }
+        }
+
+        // This part of the function only executes if there are no lines with invalid
+        // bits in the set, and it evicts using the set quee so that it uses the FIFO
+        // policy
+        Line evictedLine = queues[setIndex].remove();
+        evictedLine.setTag(tag);
+        evictedLine.setData(data);
+        queues[setIndex].add(evictedLine);
+        evictions++;
+        return;
+
+    }
+
+    private static String accessRam(String address) {
+        int index = Integer.parseInt(address, 16);
+        index = index / 8;
+        return ram.get(index);
+    }
+
+    private static String intToHex(int number) {
+        // Convert the integer to hexadecimal string
+        String hexString = Integer.toHexString(number);
+        return hexString;
     }
 
     private static int binaryToInteger(String string) {
